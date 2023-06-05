fd091cd0b352ef2ac9c5097e2634a51edf6fd3b0
Talha Mert İnözü <talmer2001@gmail.com>
2023-06-05 23:34:28 +0300

    changed request urls

--- src/main/java/com/nirengi/kapnews/controller/UserServiceController.java
+++ src/main/java/com/nirengi/kapnews/controller/UserServiceController.java
@@ -5,27 +5,24 @@ import com.nirengi.kapnews.dto.UserDto;
 import com.nirengi.kapnews.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/users")
 public class UserServiceController {
 
     @Autowired
     UserService userService;
 
 
-    @RequestMapping(value = "/users",method = RequestMethod.GET)
+    @GetMapping
     public List<UserDto> getAllUsers(){
         return  userService.getAllUsers();
     }
 
-    @RequestMapping(value = "/createUser",method = RequestMethod.POST)
+    @PostMapping(value = "/create")
     public UserDto createUser(@RequestBody @Validated UserDto userDto){
         return  userService.createUser(userDto);
     }
