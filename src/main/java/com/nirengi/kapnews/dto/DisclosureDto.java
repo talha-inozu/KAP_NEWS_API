fd12c00538e0b86dc68c030328a3481627a4d640
Talha Mert İnözü <talmer2001@gmail.com>
2023-11-05 18:42:26 +0300

    added PrepareAndSendMail for concurrency

--- src/main/java/com/nirengi/kapnews/dto/DisclosureDto.java
+++ src/main/java/com/nirengi/kapnews/dto/DisclosureDto.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+import java.lang.reflect.Field;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,5 +21,21 @@ public class DisclosureDto {
     private String title;
     private String summary;
     private String publishDate;
+
+    public String toDisclosureString() {
+        Field[] fields = DisclosureDto.class.getDeclaredFields();
+        String dto = "";
+        for (int i = 0; i < fields.length; i++) {
+            Field field = fields[i];
+            field.setAccessible(true); // Allow access to private fields
+            if (!field.getName().equals("id")) {
+                try {
+                    dto = dto + field.getName() + " : " + (String) field.get(this) + "\n";
+                } catch (Exception e) {
+                }
+            }
+        }
+        return dto;
+    }
 
 }
